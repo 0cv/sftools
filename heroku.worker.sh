3aaa2c2b276b9ca9eec52c1703f3c750e2569ec4
CV <christophe.vidal.1983@gmail.com>
2017-03-06 09:20:44 +0100

    Change Git url to a remote

--- heroku.worker.sh
+++ heroku.worker.sh
@@ -14,7 +14,7 @@ git commit -m "Heroku Build"
 
 echo push to Heroku
 #git push web dist:master --force
-git push https://git.heroku.com/sftoolsunittestworker.git dist:master --force
+git push worker dist:master --force
 
 
 git checkout master
